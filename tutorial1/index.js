885f152efc43169fed45ed859b832941627682ab
lukasBoll <ga24xeg@mytum.de>
2020-11-23 16:12:58 +0100

    exercise

---
+++ tutorial1/index.js
@@ -0,0 +1,17 @@
+//TODO: Say Hello World
+console.log("Hello Wo");
+
+//TODO implement bubblesort
+function bubbleSort(arr) {
+  /*var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;*/
+}
